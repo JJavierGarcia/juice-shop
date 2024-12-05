c3b20f379f1916b7b7eea00b7b7d7f7984fc1eaa
JJavierGarcia <hnd85543@educastur.es>
2024-12-05 21:32:58 +0100

    Update profileImageUrlUpload.ts
    Modificamos la url introducida en la linea 18 a string

--- routes/profileImageUrlUpload.ts
+++ routes/profileImageUrlUpload.ts
@@ -16,6 +16,8 @@ module.exports = function profileImageUrlUpload () {
   return (req: Request, res: Response, next: NextFunction) => {
     if (req.body.imageUrl !== undefined) {
       const url = req.body.imageUrl
+      const url = req.body.imageUrl.toString()
+      
       if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
       const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
