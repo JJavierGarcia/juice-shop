e18685089d1830b82bd734ba853d1a795f36ad32
JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>
2024-12-05 17:27:45 +0000

    Auto-fix linting issues
    Signed-off-by: JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>

--- frontend/src/app/search-result/search-result.component.ts
+++ frontend/src/app/search-result/search-result.component.ts
@@ -150,7 +150,7 @@ export class SearchResultComponent implements OnDestroy, AfterViewInit {
       this.dataSource.filter = queryParam.toLowerCase()
       // this.searchValue = this.sanitizer.bypassSecurityTrustHtml(queryParam) // vuln-code-snippet vuln-line localXssChallenge xssBonusChallenge
       this.searchValue = this.sanitizer.sanitize(queryParam)
-      
+
       this.gridDataSource.subscribe((result: any) => {
         if (result.length === 0) {
           this.emptyState = true
