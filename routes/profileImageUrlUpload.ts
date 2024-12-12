0f79e117e438caa09096e62a9899cff9a1da888b
JJavierGarcia <hnd85543@educastur.es>
2024-12-12 19:14:11 +0100

    Update profileImageUrlUpload.ts
    solucion de irene comentada

--- routes/profileImageUrlUpload.ts
+++ routes/profileImageUrlUpload.ts
@@ -15,6 +15,7 @@ const request = require('request')
 module.exports = function profileImageUrlUpload () {
   return (req: Request, res: Response, next: NextFunction) => {
     if (req.body.imageUrl !== undefined) {
+      //Solucion de Irene
       const url =new URL( req.body.imageUrl );  
       if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
       const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
